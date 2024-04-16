8a4600bcf359faedfd2267b4139b8c94fb42e5ab
selaputriismalia <88119620+selaputriismalia@users.noreply.github.com>
2024-04-16 21:09:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -113,6 +113,7 @@ st.sidebar.header('Select Number of Clusters')
 n_clusters = st.sidebar.slider('Jumlah Cluster', min_value=2, max_value=10, value=2, step=1)
 
 
+
 # Sidebar untuk menampilkan anime dengan rating tertinggi
 st.sidebar.header('Anime dengan Rating Tertinggi')
 
@@ -122,10 +123,14 @@ highest_rated_anime = df[df['rating'] == df['rating'].max()]
 # Tampilkan judul dan deskripsi anime dengan rating tertinggi di sidebar
 if not highest_rated_anime.empty:
     for index, row in highest_rated_anime.iterrows():
-        st.sidebar.subheader(row['judul'])
-        st.sidebar.write(row['deskripsi'])
+        if 'judul' in row and 'deskripsi' in row:
+            st.sidebar.subheader(row['judul'])
+            st.sidebar.write(row['deskripsi'])
+        else:
+            st.sidebar.write("Data tidak lengkap untuk anime dengan rating tertinggi.")
 else:
     st.sidebar.write('Tidak ada data untuk anime dengan rating tertinggi.')
+
 
 
 
