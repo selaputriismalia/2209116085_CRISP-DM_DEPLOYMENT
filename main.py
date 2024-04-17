f6261850057b344e96792665610bf3676e5a1f62
selaputriismalia <88119620+selaputriismalia@users.noreply.github.com>
2024-04-17 19:38:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ with col1:
 
 with col2:
   st.empty()
-  fig, ax = plt.subplots(figsize=(25, 20))
+  fig, ax = plt.subplots(figsize=(25, 26))
   sns.histplot(df['rating'], bins=20, kde=True, color='blue', ax=ax)
   ax.set_title('Distribusi Rating Anime')
   ax.set_xlabel('Rating')
